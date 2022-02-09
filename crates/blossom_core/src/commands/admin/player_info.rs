29c54a5c99df6df5e7aeab3661b546e74ac3a7c3
Rob <rob@robwagner.dev>
2022-02-09 01:45:40 -0500

    Moved all admin commands into separate modules
    All commands now use .next() or .get() to avoid out-of-bounds errors when directly indexing in.

---
+++ crates/blossom_core/src/commands/admin/player_info.rs
@@ -0,0 +1,40 @@
+use crate::{
+    command::{Command, GameCommand},
+    context::Context,
+    error::Result,
+    response::Response,
+    role::Role,
+};
+
+pub struct PlayerInfo;
+
+impl GameCommand for PlayerInfo {
+    fn create() -> Command {
+        Command {
+            name: "@player".to_string(),
+            description: "Information about a player.".to_string(),
+            ..Default::default()
+        }
+    }
+
+    fn run(ctx: Context) -> Result<Response> {
+        let player = ctx.world.get_player(ctx.id)?;
+
+        if player.account.roles.contains(&Role::Admin) {
+            match ctx.tokens.remaining.get(0) {
+                Some(name) => {
+                    if let Some(target) = ctx.world.players.iter().find(|p| p.name == *name) {
+                        return Ok(Response::Client(format!("{:#?}", target)));
+                    }
+
+                    Ok(Response::Client("Player not found.".to_string()))
+                }
+                None => Ok(Response::Client(
+                    "View player information. Usage: @player <name>".to_string(),
+                )),
+            }
+        } else {
+            ctx.world.unknown(player.id)
+        }
+    }
+}
---
+++ crates/blossom_core/src/commands/admin/world_info.rs
@@ -0,0 +1,29 @@
+use crate::{
+    command::{Command, GameCommand},
+    context::Context,
+    error::Result,
+    response::Response,
+    role::Role,
+};
+
+pub struct WorldInfo;
+
+impl GameCommand for WorldInfo {
+    fn create() -> Command {
+        Command {
+            name: "@world".to_string(),
+            description: "Information about the world state.".to_string(),
+            ..Default::default()
+        }
+    }
+
+    fn run(ctx: Context) -> Result<Response> {
+        let player = ctx.world.get_player(ctx.id)?;
+
+        if player.account.roles.contains(&Role::Admin) {
+            Ok(Response::Client(format!("{}", ctx.world)))
+        } else {
+            ctx.world.unknown(player.id)
+        }
+    }
+}
