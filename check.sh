86177283769c1b94d2bf6461ef97f996d6d16da3
Rob Wagner <rob@sombia.com>
2022-04-26 13:55:30 -0400

    Update check script

--- check.sh
+++ check.sh
@@ -3,5 +3,5 @@ cargo test -p blossom_core -p blossom_core -- --quiet
 cargo clippy --workspace -- -A clippy::single_component_path_imports -A clippy::from_over_into
 markdownlint . --disable MD033
 cd crates/blossom_web/client
-yarn fmt
-yarn lint
+npm run fmt
+npm run lint
