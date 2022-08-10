49a7e068494298dfe377e47941fef3b2f14dea13
Rob Wagner <rob@sombia.com>
2022-08-10 18:19:17 -0400

    Swap yarn with npm

--- check.sh
+++ check.sh
@@ -1,7 +1,6 @@
 cargo fmt --all
 cargo test -p blossom_core -p blossom_core -- --quiet
 cargo clippy --workspace -- -A clippy::single_component_path_imports -A clippy::from_over_into
-markdownlint . --disable MD033
 cd crates/blossom_web/client
-yarn fmt
-yarn lint
+npm run fmt
+npm run lint
