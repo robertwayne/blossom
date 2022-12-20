ddf7099b489278e7f0be642b3a4d8074abf4d182
Rob Wagner <rob@sombia.com>
2022-12-19 19:43:02 -0500

    Fix check script to work with new structure

--- check.sh
+++ check.sh
@@ -1,6 +1,7 @@
 cargo fmt --all
-cargo test -p blossom_core -p blossom_core -- --quiet
+cargo test -p blossom -p blossom -- --quiet
 cargo clippy --workspace -- -A clippy::single_component_path_imports
-cd blossom-web/client
+cargo sqlx prepare --check --merged
+cd blossom-dashboard
 npm run fmt
 npm run lint
