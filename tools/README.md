2ef213352bf3e63a7c98425fd8ddde5f03b80f46
Rob Wagner <rob@sombia.com>
2023-03-01 18:12:27 -0500

    Update swarm script

--- tools/README.md
+++ tools/README.md
@@ -10,3 +10,17 @@ run, use the command `python <tool>.py`. You can view a tools help menu with
 | swarm     | [swarm.py](/tools/swarm.py)         | Load testing and command 'fuzzing' script. |
 | make_room | [make_room.py](/tools/make_room.py) | Generates a formatted room file.           |
 <!-- markdownlint-enable -->
+
+## Using Swarm
+
+In order to use swarm, you must first run `python swarm.py create n`, where `n`
+is how many players it will create. They will NOT be logged in when finished.
+
+After that, you can run `python swarm.py run n`, where `n` is the same number
+before - or the total amount of players you have created as a whole, to start
+the swarm. It will take a bit to log them all in, depending on how many players
+you are running.
+
+All the players you create are stored in `swarm_names.json` in the `tools`
+directory. This must be cleared if you wipe the database, otherwise the script
+will try logging in with players that no longer exist.
--- tools/swarm.py
+++ tools/swarm.py
@@ -7,6 +7,7 @@ from multiprocessing import Process
 from telnetlib import ECHO, IAC, WILL, WONT, Telnet
 
 PROCESS_COUNT = 6
+WAIT_TIME = 0.5
 
 names = []
 commands = ['n', 'e', 's', 'w', 'd', 'u', 'say hello', 'global hello', 'who', 'afk', 'brief', 'look']
@@ -28,32 +29,31 @@ def create_swarm(n):
     # Creates a pool of n connections.
     connections = [Telnet('localhost', 5000) for _ in range(int(n))]
 
-    # For each connection, create a new player with the name `Swarm_n` and the password `password`.
+    # For each connection, create a new player with the name `Swarm_n` and the
+    # password `password`.
     for i, tn in enumerate(connections):
         random_name = random_string()
 
-        tn.read_until(b'What is your name? If you are new, enter the name you wish to use.')
         name = f'{random_name}'
+        time.sleep(1)
         tn.write(name.encode('ascii') + b'\r\n')
 
-        time.sleep(0.1)
-        tn.read_until(b'Character not found. Create a new character with this name? [Y/n]')
+        time.sleep(1)
         response = 'y'
         tn.write(response.encode('ascii') + b'\r\n')
 
-        time.sleep(0.1)
+        time.sleep(1)
         tn.get_socket().send(IAC + WONT + ECHO)
 
-        time.sleep(0.1)
-        tn.read_until(b'What will your password be?')
+        time.sleep(1)
         password = 'password'
         tn.write(password.encode('ascii') + b'\r\n')
 
-        time.sleep(0.1)
+        time.sleep(1)
         tn.get_socket().send(IAC + WILL + ECHO)
 
         tn.write('quit'.encode('ascii') + b'\r\n')
-        time.sleep(0.1)
+        time.sleep(1)
 
 def run_swarm(chunk):
     print(f'Swarming the server with {len(chunk)} players.')
@@ -61,35 +61,44 @@ def run_swarm(chunk):
     # Creates a pool of n connections.
     connections = [Telnet('localhost', 5000) for _ in range(len(chunk))]
 
-    # For each connection, logs in with the name `Swarm_n` and the password `password`. Note that
-    # you need to have run `python swarm.py create n` before running this.
+    # For each connection, logs in with the name `Swarm_n` and the password
+    # `password`. Note that you need to have run `python swarm.py create n`
+    # before running this.
     for i, tn in enumerate(connections):
-        tn.read_until(b'What is your name? If you are new, enter the name you wish to use.')
+        time.sleep(1)
         name = names[i]
         tn.write(name.encode('ascii') + b'\r\n')
 
-        time.sleep(0.1)
+        print(f'Logging in as {name}.')
+
+        time.sleep(1)
         tn.get_socket().send(IAC + WONT + ECHO)
 
-        time.sleep(0.1)
-        tn.read_until(b'What is your password?')
+        time.sleep(1)
         password = 'password'
         tn.write(password.encode('ascii') + b'\r\n')
 
-        time.sleep(0.1)
+        print(f'Logged in as {name}.')
+
+        time.sleep(1)
         tn.get_socket().send(IAC + WILL + ECHO)
 
-    # For each connection, randomly sends a commands (from the list at the top of this file).
+    print('All players logged in.')
+
+    # For each connection, randomly sends a commands (from the list at the top
+    # of this file).
     while True:
         for c in connections:
             dir = random.choice(commands)
             c.write(dir.encode('ascii') + b'\r\n')
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         
 def print_missing_args_error(name):
     print(f'Error: Missing argument. \
     \nUsage: python swarm.py {name} <n> \
     \n\nSee python swarm.py --help for more information.')
+
+    sys.exit(1)
 
 
 if __name__ == '__main__':
